5e6d1c9586c74b7e5f7a909c9cb45a393bc63ad9
Mrunal Patel <mrunalp@gmail.com>
2016-05-20 15:04:24 -0700

    Add support for copying directories to gocp
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- cmd/gocp/main.go
+++ cmd/gocp/main.go
@@ -12,7 +12,18 @@ func main() {
 		log.Fatalf("usage: gocp <src> <dest>")
 	}
 
-	if err := fileutils.CopyFile(os.Args[1], os.Args[2]); err != nil {
-		log.Fatalf("error copying %s to %s: %v", os.Args[1], os.Args[2], err)
+	fi, err := os.Stat(os.Args[1])
+	if err != nil {
+		log.Fatalf("failed to stat file :%v", err)
+	}
+
+	if fi.IsDir() {
+		if err := fileutils.CopyDirectory(os.Args[1], os.Args[2]); err != nil {
+			log.Fatalf("error copying %s to %s: %v", os.Args[1], os.Args[2], err)
+		}
+	} else {
+		if err := fileutils.CopyFile(os.Args[1], os.Args[2]); err != nil {
+			log.Fatalf("error copying %s to %s: %v", os.Args[1], os.Args[2], err)
+		}
 	}
 }
